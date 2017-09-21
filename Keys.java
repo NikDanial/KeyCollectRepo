f3e6fe1c00b39d1dbb49ca92c9cd384f83033ace
ник даниал <evilskin69@gmail.com>
2017-09-21 08:51:08 +0800

    Update to Keys.java
    Added comments

--- Keys.java
+++ Keys.java
@@ -1,25 +1,56 @@
+/**
+ * class for Keys object 
+ * @author Arif
+ * @param none
+ * @return none
+ */
 public class Keys {
 	Point key_pos = new Point();
 	int key_code;
 	
+	/** 
+	 * default constructor for Keys
+	 */
 	public Keys(){
 		key_pos = new Point();
 		key_code = 0;
 	}
 	
+	/** 
+	 * overloaded constructor for Keys
+	 */
 	public Keys(Point pos, int code){
 		this.key_pos = pos;
 		this.key_code = code;
 	}
 	
+	/**
+	 * overloaded constructor for Keys
+	 * @param pos the position of the key
+	 */
 	public Keys(Point pos){
 		this.key_pos = pos;
 		key_code = 0;
 	}
 	
+	/**
+	 * get method for key_pos
+	 */
 	public Point getKeyPos() { return key_pos; }
+	/**
+	 * set method for key_pos
+	 * @param pos the new position of the key
+	 */
 	public void setKeyPos(Point pos) { this.key_pos = pos; }
+	
+	/**
+	 * get method for key_code
+	 */
 	public int getKeyCode() { return key_code; }
+	/**
+	 * set method for key_code
+	 * @param pos the new value of the key
+	 */
 	public void setKeyCode(int code) { this.key_code = code; } 
 	
 }
