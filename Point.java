5a5dd7efe6f9df8eb4883988e9e9717599ed0a72
ник даниал <evilskin69@gmail.com>
2017-09-21 09:11:40 +0800

    Update Point.java
    edit comments

--- Point.java
+++ Point.java
@@ -14,6 +14,7 @@ public class Point {
 	public Point(){ x=0; y=0; }
 	
 	/**
+	 * @author Arif
 	 * overloaded constructor for Point
 	 * @param x the x value of the point
 	 * @param y the y value of the point
@@ -25,19 +26,23 @@ public class Point {
 	}
 	
 	/**
+	 * @author Arif
 	 * get method for x
 	 */
 	public int getX(){ return x; }
 	/** 
+	 * @author Arif
 	 * set method for x
 	 * @param x the new x value
 	 */
 	public void setX(int x){ this.x = x; }
 	/**
+	 * @author Arif
 	 * get method for y
 	 */
 	public int getY(){ return y; }
 	/** 
+	 * @author Arif
 	 * set method for y
 	 * @param y the new y value
 	 */
