8e4ff30087475b46e59ea7024355b1fe05ea480a
ник даниал <evilskin69@gmail.com>
2017-09-21 08:53:23 +0800

    Create KeyCollectDriver.java
    and additional class for latest update

---
+++ KeyCollectDriver.java
@@ -0,0 +1,12 @@
+/**
+ * driver class for the program 
+ * @author Nik
+ * @param none
+ * @return none
+ */
+ 
+public class KeyCollectDriver {
+	public static void main(String[] args){
+		KeyCollect k = new KeyCollect();
+	}
+}
