4ed07256bafc0fd6d67818a67c2e5573dde77ea8
ник даниал <evilskin69@gmail.com>
2017-09-21 09:13:17 +0800

    Update TutorialTexts.java
    edit comments

--- TutorialTexts.java
+++ TutorialTexts.java
@@ -1,6 +1,6 @@
 /**
  * class for TutorialTexts to store texts in tutorial section 
- * @author Aneek
+ * @author Nik
  * @param none
  * @return none
  */
@@ -16,19 +16,74 @@ public class TutorialTexts{
 	
 	private String instruct1 =  "  There are 4 Playes. To win, a player needs to collect all 5 keys and"; 
 	private String instruct2 =  "  go to the middle of the board to unlock the treasure chest.";
-    private String instruct3 =  "  The players take turns to move. Initially each player can only move"; 
+        private String instruct3 =  "  The players take turns to move. Initially each player can only move"; 
 	private String instruct4 =  "  1 or 2 squares in any direction at a time. When they collect a key,"; 
-    private String instruct5 =  "  the last key collected restricts their movement :";
+        private String instruct5 =  "  the last key collected restricts their movement :";
 
+	/**
+	 * get method for given variable
+	 * @author Nik
+	 * @return pinkeyText 
+	 */
 	public String getPinkeyText() { return pinkeyText; }
+	/**
+	 * get method for given variable
+	 * @author Nik
+	 * @return donkeyText
+	 */
 	public String getDonkeyText() { return donkeyText; }
+	/**
+	 * get method for given variable
+	 * @author Nik
+	 * @return keydiskText
+	 */
 	public String getKeydiskText() { return keydiskText; }
+	/**
+	 * get method for given variable
+	 * @author Nik
+	 * @return keynoteText
+	 */
 	public String getKeynoteText() { return keynoteText; }
+	/**
+	 * get method for given variable
+	 * @author Nik
+	 * @return monkeyText
+	 */
 	public String getMonkeyText() { return monkeyText; }
+	/**
+	 * get method for given variable
+	 * @author Nik
+	 * @return space 
+	 */
 	public String getSpace() { return space; }
+	/**
+	 * get method for given variable
+	 * @author Nik
+	 * @return instruct1 
+	 */
 	public String getInstruct1() { return instruct1; }
+	/**
+	 * get method for given variable
+	 * @author Nik
+	 * @return instruct2
+	 */
 	public String getInstruct2() { return instruct2; }
+	/**
+	 * get method for given variable
+	 * @author Nik
+	 * @return instruct3
+	 */
 	public String getInstruct3() { return instruct3; }
+	/**
+	 * get method for given variable
+	 * @author Nik
+	 * @return instruct4
+	 */
 	public String getInstruct4() { return instruct4; }
+	/**
+	 * get method for given variable
+	 * @author Nik
+	 * @return instruct5
+	 */
 	public String getInstruct5() { return instruct5; }
 }
