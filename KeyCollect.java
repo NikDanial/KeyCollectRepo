59d5e0d7cd6f56c6b566a69dc0c45d81988a125b
ник даниал <evilskin69@gmail.com>
2017-09-21 08:50:09 +0800

    Update to KeyCollect.java
    Added comments and some edits

--- KeyCollect.java
+++ KeyCollect.java
@@ -11,46 +11,69 @@ import java.awt.Window;
 import javax.swing.ImageIcon;
 import static java.lang.Math.*;
 
+/**
+ * class KeyCollect that configures the 9x9 board with its respective components
+ * @author Nik, Aneek, Arif
+ * @param none
+ * @return none
+ */
 public class KeyCollect extends JFrame implements ActionListener {
 	
+	/** creates panels npl(panel for 9x9 board), dialog_pnl(panel for scoreboard), tuto_pnl(panel for tutorial dialog box),
+	 * and tuto_text_pnl (panel for tutorial text box 
+	 */
 	JPanel npl = new JPanel();
 	JPanel dialog_pnl = new JPanel();
-	JDialog jdl = new JDialog();
-	//JDialog windlg = new JDialog();
+	JPanel tuto_pnl = new JPanel();
+	JPanel tuto_text_pnl = new JPanel();
 	
+	/** creates dialog boxes for scoreboard, tutorial dialog, and tutorial text */
+	JDialog jdl = new JDialog();
+	JDialog tutorial = new JDialog(); 
+	JDialog tuto_text = new JDialog();
+	
+	/** creates a 9x9 JButton array for the 9x9 board and
+	 *  4x6 JButton array for the scoreboard
+	 */
 	JButton[][] buttons = new JButton[9][9];
 	JButton[][] buttons_jdialog = new JButton[4][6];
 	
+	/** creates 4 player objects, 6 key objects (0 for null key) and a new TutorialTexts object */
 	Player[] players =  new Player[4];
 	Keys[] keys = new Keys[6];
+	TutorialTexts tt = new TutorialTexts();
 	
+	/** creates image icons and image icon arrays to add to buttons */
 	ImageIcon P1,P2,P3,P4,pinkey,donkey,keydisk,keynote,monkey,chest;
 	ImageIcon[] player_images = new ImageIcon[4];
 	ImageIcon[] key_images = new ImageIcon[6];
 	
-	int[][] binary_matrix = new int[9][9]; /* 0:empty 1:occupied */
+											
+	/** 9x9 binary matrix for logic. 0:empty, 1:occupied */
+	int[][] binary_matrix = new int[9][9]; 
+	/** store the old coordinates, to be compared with the new, i.e i and j */
+	Point[] temps = new Point[4]; 
 	
-	int which_player_turn = 0; /* indicate who's turn is it for the actionlistener block */
-	Point[] temps = new Point[4]; /* store the old coordinates, to be compared with the new, i.e i and j */
-	
-	double d;
-	int which_key, winner;
+	double d; /** distance calculation */
+	int which_key, winner; /** which key is selected, and which is the winner */
 	double max_dist;
 	boolean you_shall_pass;
 	boolean at_key;
 	boolean revert_to_key_image;
 	int temp_key;
-	
+	int which_player_turn = 0; /* indicate who's turn is it for the actionlistener block */
 	int which_player_turn_clone =0;
 
 	
-	
-	public static void main(String[] args){
-		KeyCollect k = new KeyCollect();
-	}
-	
+	/**
+	* class KeyCollect default constructor
+	* @author Nik, Aneek, Arif
+	* @param none
+	* @return none
+	*/
 	public KeyCollect(){
 
+	    /** Creates the Board JFrame */
 		super("Key Collector");
 		setSize(600,600);
 		setResizable(false);
@@ -59,18 +82,68 @@ public class KeyCollect extends JFrame implements ActionListener {
 		npl.setLayout(new GridLayout(9,9));
 		dialog_pnl.setLayout(new GridLayout(4,6));
 		
+		/** Creates the Scoreoard JDialog */
 		jdl.setTitle("SCOREBOARD");
 		jdl.setSize(400,300);
 		jdl.setResizable(false);
 		jdl.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		jdl.setLocation(900,200);
+		jdl.setLocation(900,80);
+		
+		/** Creates the Tutorial JDialog */
+		tutorial.setTitle("TUTORIAL");
+		tutorial.setSize(400,100);
+		tutorial.setLayout(new FlowLayout());
+		tutorial.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		tutorial.setLocationRelativeTo(null);
+		
+		/** Creates the Tutorial text JDialog */
+		tuto_text.setTitle("TUTORIAL");
+		tuto_text.setSize(450,250);
+		tuto_text.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		tuto_text.setLocation(895,380);
+		
+		/** Adding text from TutorialTexts class into tuto_text_pnl */
+		tuto_text_pnl.setLayout(new BoxLayout(tuto_text_pnl, BoxLayout.Y_AXIS));
+		tuto_text_pnl.add(new JLabel(tt.getSpace()));
+		tuto_text_pnl.add(new JLabel(tt.getInstruct1()));
+		tuto_text_pnl.add(new JLabel(tt.getInstruct2()));
+		tuto_text_pnl.add(new JLabel(tt.getInstruct3()));
+		tuto_text_pnl.add(new JLabel(tt.getInstruct4()));
+		tuto_text_pnl.add(new JLabel(tt.getInstruct5()));
+		tuto_text_pnl.add(new JLabel(tt.getSpace()));
+		tuto_text_pnl.add(new JLabel(tt.getPinkeyText()));
+		tuto_text_pnl.add(new JLabel(tt.getDonkeyText()));
+		tuto_text_pnl.add(new JLabel(tt.getKeydiskText()));
+		tuto_text_pnl.add(new JLabel(tt.getKeynoteText()));
+		tuto_text_pnl.add(new JLabel(tt.getMonkeyText()));
+		tuto_text.add(tuto_text_pnl);
+		
+		tuto_pnl.add(new JLabel("Do you wish to view the tutorial?     "));
+		JButton yes = new JButton("YES");
+		JButton no = new JButton("NO");
+		
+		/** if Yes selected, go to actionPerformed defintion */
+		yes.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                yesActionPerformed(evt);
+            }
+        });
+		/** if No selected, go to actionPerformed defintion */
+		no.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				noActionPerformed(evt);
+			}
+		});
+		
+		tuto_pnl.add(yes); /** add yes and no buttons to tutorial JDialog */
+		tuto_pnl.add(no);
+		tutorial.add(tuto_pnl);
 		
 		temp_key = 0;
-		
-		
 		dialog_pnl.setLayout(new GridLayout(4,6));
 		
-		/* add 9x9 grid of buttons with 81 actionlisteners */
+
+		/** add 9x9 grid of buttons with 81 actionlisteners */
 		for (int i=0;i<9;i++){
 			for (int j=0;j<9;j++){
 				buttons[i][j] = new JButton();
@@ -93,15 +166,14 @@ public class KeyCollect extends JFrame implements ActionListener {
 		add(npl);
 		jdl.setVisible(true);
 		
-		
-		/* initialize the binary matrix */
+		/** initialize the binary matrix */
 		for (int i=0;i<9;i++){
 			for (int j=0;j<9;j++){
 				binary_matrix[i][j] = 0;
 			}
 		}
 		
-		
+		/** set the image icons to the gif images */
 		P1=new ImageIcon(this.getClass().getResource("1.gif"));
 		P2=new ImageIcon(this.getClass().getResource("2.gif"));
 		P3=new ImageIcon(this.getClass().getResource("3.gif"));
@@ -113,7 +185,7 @@ public class KeyCollect extends JFrame implements ActionListener {
 		keynote=new ImageIcon(this.getClass().getResource("d.gif"));
 		monkey=new ImageIcon(this.getClass().getResource("e.gif"));
 		
-		/* put images into a neat array for player and keys */
+		/** put images into a neat array for player and keys */
 		player_images[0] = P1;
 		player_images[1] = P2;
 		player_images[2] = P3;
@@ -126,21 +198,23 @@ public class KeyCollect extends JFrame implements ActionListener {
 		key_images[4] = keynote;
 		key_images[5] = monkey;
 		
-		/* shuffle initial player positions */
+		/** creates indexes for all 4 players to be shuffled for random placement on the board */
 		List<Integer> solution = new ArrayList<Integer>();
 		for (int i = 0; i < 4; i++) 
 			solution.add(i);
 		
+		/** shuffle initial player positions */
 		Collections.shuffle(solution);
 	    Point[] butts = new Point[4];
-	 	butts[0] = new Point(0,0); //initial player positions
+	 	butts[0] = new Point(0,0); /** initial player positions */
 		butts[1] = new Point(0,8);
 		butts[2] = new Point(8,0);
 		butts[3] = new Point(8,8);
 		
+		/** set 4 players' initial coordinates */
 		for (int i=0;i<4;i++){
 			if (solution.get(i) == 0){
-				players[0] = new Player(butts[i]); //set 4 players' initial coordinates
+				players[0] = new Player(butts[i]); 
 			}
 			else if (solution.get(i) == 1){
 				players[1] = new Player(butts[i]);
@@ -153,47 +227,32 @@ public class KeyCollect extends JFrame implements ActionListener {
 			}
 		} 
 	
-	    /* set the images corresponding to each player, now that coordinates are set */
+	    /** set the images corresponding to each player, now that coordinates are set */
+		buttons[4][4].setIcon(chest); 
+		
 		buttons[players[0].player_pos.getX()][players[0].player_pos.getY()].setIcon(P1);
 		buttons[players[1].player_pos.getX()][players[1].player_pos.getY()].setIcon(P2);
 		buttons[players[2].player_pos.getX()][players[2].player_pos.getY()].setIcon(P3);
 		buttons[players[3].player_pos.getX()][players[3].player_pos.getY()].setIcon(P4);
-
-		buttons[4][4].setIcon(chest); 
 		
 		buttons_jdialog[0][0].setIcon(P1);
 		buttons_jdialog[1][0].setIcon(P2);
 		buttons_jdialog[2][0].setIcon(P3);
 		buttons_jdialog[3][0].setIcon(P4);
 		
-		
-		
-		/* useless debugging output */
-		System.out.println("Player 1 point : (" + players[0].player_pos.getX() + "," + players[0].player_pos.getY() + ")");
-		System.out.println("Player 2 point : (" + players[1].player_pos.getX() + "," + players[1].player_pos.getY() + ")");
-		System.out.println("Player 3 point : (" + players[2].player_pos.getX() + "," + players[2].player_pos.getY() + ")");
-		System.out.println("Player 4 point : (" + players[3].player_pos.getX() + "," + players[3].player_pos.getY() + ")");
-		
-		/* set 1 to in bin matrix where space is taken by player*/
+		/** set 0 to 1 in binary matrix where space is taken by player*/
 		for (int i=0;i<4;i++)
 			binary_matrix[players[i].player_pos.getX()][players[i].player_pos.getY()] = 1;
 		
-		binary_matrix[4][4] = 1; /* for chest */
+		binary_matrix[4][4] = 1; /** for chest */
 		
-		/* print bin matrix for debugging */
-		for (int i=0;i<9;i++){
-			for (int j=0;j<9;j++)
-				System.out.print(binary_matrix[i][j] + " ");
-			System.out.println("");
-		}
-		
-		/* initialize temps to store the initial (old) coordinates of players */
+		/** initialize temps to store the initial (old) coordinates of players */
 		for (int i=0;i<4;i++)
 			temps[i] = new Point(players[i].player_pos.getX(), players[i].player_pos.getY());
 		
 		Random rand = new Random();
 
-		/* assigns keys to locations that don't overlap */
+		/** assigns keys to locations that don't overlap */
 		int randX, randY;
 		for (int i=1;i<6;i++){
 			while (true)
@@ -210,110 +269,103 @@ public class KeyCollect extends JFrame implements ActionListener {
          
 		}
 		keys[0] = new Keys();
+		
 		/* initialize key codes */
 		for (int i=0;i<6;i++) { keys[i].setKeyCode(i); }
 		
 		buttons_jdialog[which_player_turn][0].setBackground(Color.blue);
-
-		
 		setVisible(true);
+		tutorial.setVisible(true);
 		
 		
 	}
-	
+	/** actionListener event definition for button press event in 9x9 grid
+	 * @param ae the event thrown from button press in 9x9 grid
+	 * @return none
+	 */
+	@Override
 	public void actionPerformed(ActionEvent ae) {
 		   
 		   which_player_turn %= 4;
-		  
 		   
-		   
-		   
-		   
-		   
-		   System.out.println("Player " + which_player_turn + " now has key " + players[which_player_turn].getPlayerKey());
-		   System.out.println("Player " + which_player_turn + " now has " + players[which_player_turn].getNoKeys() + " keys");
-		   
-		   Object source = ae.getSource();    
+		   Object source = ae.getSource(); 
 		   for (int i=0;i<9;i++){
 			   for (int j=0;j<9;j++){
-				   if (source == (buttons[i][j])){
+				   if (source == (buttons[i][j])){ /** if the source of the click is equals to that particular button */
 					   
-					   if ( (players[which_player_turn].getNoKeys() >= 5))
+					   if ( (players[which_player_turn].getNoKeys() >= 5)) /** if the current player has >= 5 keys, chest location is accessible */
 						     binary_matrix[4][4] = 0;
 							
 						
 					   System.out.println("-----------------------------------------");
 					   System.out.println("Button " + i + " " + j + " pressed");
 					   
-					      
+					      /** Initial default move restriction (no keys yet) */
 						  if (players[which_player_turn].getPlayerKey() == 0){
-								/** REGULAR MOVE **/
 								max_dist = 3; //actually 2, but 2.9 also count
 								System.out.println("Maximum 2 spaces");
 						  }
+						  /** current key is Pinkey */
 						  else if (players[which_player_turn].getPlayerKey() == 1){
-							  /** PINKEY **/
 							    max_dist = 1.5;
 								System.out.println("Maxium 1 space");
 						  }
+						   /** current key is Donkey */
 						  else if (players[which_player_turn].getPlayerKey() == 2){
-							  /** DONKEY **/
 							  max_dist = 4.5;
 							  System.out.println("Maximum 3 spaces");
 						  }
+						   /** current key is Keydisk */
 						  else if (players[which_player_turn].getPlayerKey() == 3){
-							  /** KEYDISK **/
 							  max_dist = 4;
 							  System.out.println("Maximum 3 spaces");
 						  }
+						   /** current key is Keynote */
 						  else if (players[which_player_turn].getPlayerKey() == 4){
-							  /** KEYNOTE **/
 							  max_dist = 3;
 							  System.out.println("More than 1 space, maximum 2");
 						  }
+						   /** current key is Monkey */
 						  else if (players[which_player_turn].getPlayerKey() == 5){
-							  /** MONKEY **/
 							    max_dist = 5; //some random number. not needed 
 								System.out.println("Maximum 3 spaces");
 						  }
 							  
-					   
+					     /** calculate the distance between the player location and the click whether it is within range */
 					     d = sqrt( pow((i - temps[which_player_turn].getX()),2) + pow((j - temps[which_player_turn].getY()),2) );
 						 System.out.println("Distance is : " + d);
 						 
                          you_shall_pass = false;
 						 at_key = false;
+						 
+						 /** checks whether the location clicked is occupied, and the new location is within distance */
 					     if ( ((binary_matrix[i][j] == 0) || (binary_matrix[i][j] == 2)) && (d < max_dist)){
 							 
+							 /** For KEYDISK */
 							 if (players[which_player_turn].getPlayerKey() == 3)
 							 {
-								 /** KEYDISK **/
-								 /* checks that either x-axis or y-axis of the new coordinate is the same with old coordinate */
+								 /** checks that either x-axis or y-axis of the new coordinate is the same with old coordinate */
 								 if ( (i == temps[which_player_turn].getX()) || (j == temps[which_player_turn].getY()) )
 								    you_shall_pass = true;
 									
 							 }
-			
+							 /** For DONKEY */
 							 else if (players[which_player_turn].getPlayerKey() == 2)
 							 {
-								 /** DONKEY **/
 								 /* checks that both the x-axis and y-axis of the new coordinate are different than old coordinate */
 								 if ( (i != temps[which_player_turn].getX()) && (j != temps[which_player_turn].getY()) )
 								     you_shall_pass = true;
 							 }
-							 
+							 /** For KEYNOTE */
 							 else if (players[which_player_turn].getPlayerKey() == 4)
 							 {
-								 /** KEYNOTE **/
 								 if (d > 1.5)
 									 you_shall_pass = true;
 							 }
-							 
+							 /** For MONKEY */
 							 else if (players[which_player_turn].getPlayerKey() == 5)
 							 {
-								 /** MONKEY **/
-								 
-								 /** if the new position is NOT diagonal **/
+								 /** if the new position is NOT diagonal */
 								 if ( (i == temps[which_player_turn].getX()) || (j == temps[which_player_turn].getY()) ){
 									 if (d < 4)
 										 you_shall_pass = true;
@@ -331,7 +383,7 @@ public class KeyCollect extends JFrame implements ActionListener {
 
 									if ( (players[which_player_turn].getNoKeys() >= 5) && (i==4) && (j==4) ){
 										JOptionPane.showMessageDialog(this, "Player " +  (which_player_turn+1) + " WINS!");
-										/* quit the program */
+										/** quit the program */
 									}
 									
 									for (int z=0;z<4;z++)
@@ -342,9 +394,10 @@ public class KeyCollect extends JFrame implements ActionListener {
 										which_player_turn_clone = -1;
 									buttons_jdialog[which_player_turn_clone+1][0].setBackground(Color.blue);
 									
-									/* when player lands on a key, updates the player_key increase no_keys by 1 **/ 
+									/** when player lands on a key, updates the player_key increase no_keys by 1 */ 
 									which_key = 0;
 									for (int k=1;k<6;k++){
+										/** checks if new location is a location of a key */
 										if ((i == keys[k].key_pos.getX()) && (j == keys[k].key_pos.getY())){
 											at_key = true;
 											System.out.println("key " + k + " at position : " + keys[k].key_pos.getX() + "," + keys[k].key_pos.getY() );
@@ -387,24 +440,23 @@ public class KeyCollect extends JFrame implements ActionListener {
 									
 							        System.out.println("Player " + which_player_turn + " is at coordinates (" + players[which_player_turn].player_pos.getX() + "," + players[which_player_turn].player_pos.getY() + ")");
 							 
-									/* set the old location to 0 */
+									/** set the old location to 0 */
 									binary_matrix[temps[which_player_turn].getX()][temps[which_player_turn].getY()] = 0;
 									/* set the new location to 1 */
 									binary_matrix[i][j] = 1;
 						
 									
-									/* change the new location image */
+									/** change the new location image */
 									buttons[i][j].setIcon(player_images[which_player_turn]);
-									/* nullify old location image */
+									/** nullify old location image */
 									buttons[temps[which_player_turn].getX()][temps[which_player_turn].getY()].setIcon(null);
 									
 									for (int q=1;q<6;q++){
 										for (int a=0;a<4;a++){
+											/** replace the previous icons with what they were before. Not that important to understand */
 											if ( (players[a].player_pos.getX() == keys[q].key_pos.getX()) && (players[a].player_pos.getY() == keys[q].key_pos.getY()) ){
 												buttons[players[a].player_pos.getX()][players[a].player_pos.getY()].setIcon(player_images[a]);
-												System.out.println("IMAGE CHANGED COMRADE");
 												break;
-												
 											}
 											else
 												buttons[keys[q].key_pos.getX()][keys[q].key_pos.getY()].setIcon(key_images[q]);
@@ -414,7 +466,7 @@ public class KeyCollect extends JFrame implements ActionListener {
 									}
 								
 									
-									/* set the old coordinates to current coordinates */
+									/** set the old coordinates to current coordinates */
 									temps[which_player_turn].setX(i);
 									temps[which_player_turn].setY(j);
 									
@@ -433,5 +485,26 @@ public class KeyCollect extends JFrame implements ActionListener {
 		   
 	 
 	}
+	/** defintion for yes button actionPerformed event
+	 * @author nik
+	 * @param evt event of yes button clicked
+	 */
+	private void yesActionPerformed(ActionEvent evt) {                                           
+		System.out.println("YES IS CLICKED");
+		tutorial.dispose();
+		tuto_text.setVisible(true);
+        
+    }      
+	/** defintion for no button actionPerformed event
+	 * @author nik
+	 * @param evt event of no button clicked
+	 */
+	private void noActionPerformed(ActionEvent evt) {                                           
+		System.out.println("NO IS CLICKED");
+		tutorial.dispose();
+        
+    }      
+	
+	
 	
 }
